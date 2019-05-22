9c4311699464ff7812cc844e7416527767011a44
NaJaeMin92 <37066691+NaJaeMin92@users.noreply.github.com>
2019-05-23 00:59:12 +0900

    Create tutorial.py

---
+++ tutorial.py
@@ -0,0 +1,130 @@
+"""
+We will do the following steps in order:
+1. Load and normalizing the CIFAR10 training and test datasets using 'torchvision'
+2. Define a Convolutional Neural Network
+3. Define a loss function
+4. Train the network on the training data
+5. Test the network on the test data
+"""
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import copy
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+############################################
+# 1. Loading and normalizing CIFAR-10
+############################################
+
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1].
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=8)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=8)
+
+
+############################################
+# 2. Define a Convolutional Neural Network
+############################################
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net().to(device)
+
+############################################
+# 3. Define a Loss function and optimizer
+############################################
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+############################################
+# 4. Train the network
+############################################
+def train():
+    best_acc = 0
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader):
+            # get the inputs
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[{}, {}] loss: {:.4f}'.format(epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+        # save the best model
+        test_acc = test()
+        if test_acc > best_acc:
+            best_acc = test_acc
+            best_model = copy.deepcopy(net)
+            
+    torch.save(best_model.state_dict(), 'best_model.pt')    
+    print('Finished Training')
+
+
+############################################
+# 5. Test the network on the test data
+############################################
+def test():
+    correct = 0
+    total = 0
+    accuracy = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            accuracy = 100 * correct / total
+    print('Accuracy on test images: {:.2f}%'.format(accuracy))
+    return accuracy
+
+
+if __name__ == '__main__':
+    train()
